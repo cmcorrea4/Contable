4d89b96a9d73d39351774254e78f7e57ff8356b7
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2026-04-17 09:57:17 -0500

    Update Inicio.py

--- Inicio.py
+++ Inicio.py
@@ -8,7 +8,7 @@ st.set_page_config(
     page_title="Portal Contable",
     page_icon="🏢",
     layout="wide",
-    initial_sidebar_ebar_state="expanded"
+    initial_sidebar_state="expanded"
 )
 
 # ── Estilos CSS ──────────────────────────────────────────────────────────────
